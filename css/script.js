a38dc9e41e3bb757664b1669b098a00d3368efbf
DoniaShehata <82147474+DoniaShehata@users.noreply.github.com>
2023-03-19 00:11:18 +0200

    Add files via upload

---
+++ css/script.js
@@ -0,0 +1,52 @@
+// // built in string methods
+// // length 1
+// //  zero based index 
+//      //  01234567891011
+
+
+// // console.log(str.length)
+// // console.log(str[2])
+// // console.log(str.charAt(2))
+
+// // console.log(str.toLowerCase())
+// // console.log(str.toUpperCase())
+// // trim
+// // console.log(str.trim())
+// // console.log(str.trimStart())
+// // console.log(str.trimEnd())
+// // replace
+// // console.log(str.replace('e','f'))
+// // console.log(str.replaceAll('e','f'))
+// // repeat
+// // console.log(str.repeat(4))
+
+// // console.log(str.trim().charAt(2).toUpperCase().repeat(3))
+
+// // slice (start,end not included)
+
+// // console.log(str.slice(1))
+// // console.log(str.slice(1,4))
+// // console.log(str.slice(-3,-1))
+
+// // substring()
+// // console.log(str.substring(1))
+// // console.log(str.substring(1,4))
+// // console.log(str.substring(-1))
+// var str="webdesign"
+// // split  => array 
+
+// // console.log(str.split("|"))
+
+
+// // includes  startwith endwith  true or false
+
+// // console.log(str.includes('n',6))
+// // console.log(str.startsWith('d',3))
+// // console.log(str.endsWith('t'))
+
+
+// cruds
+// c=> creat add
+// r => retrive display
+// d => delete
+// s => search
